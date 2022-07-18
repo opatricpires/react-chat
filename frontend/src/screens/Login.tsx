13b74e5e9453b31282d5a6f15ca2822acff6add2
opatricpires <patric.pires@outlook.com>
2022-07-18 00:33:57 -0300

    refactor(login): change login styles

--- frontend/src/screens/Login.tsx
+++ frontend/src/screens/Login.tsx
@@ -4,30 +4,46 @@ import { useState } from "react";
 
 export const Login = () => {
   const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
   const { login } = useChannelContext();
 
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="w-full h-screen bg-zinc-900 flex justify-center items-center">
       <form
         onSubmit={(e) => {
           e.preventDefault();
           login(userName);
           navigate("/channels");
         }}
+        className="w-80 h-96 bg-slate-50 px-4 py-6 rounded-md flex flex-col"
       >
-        <label>Username</label>
         <input
           type="text"
           name="username"
           id="username"
-          placeholder="Seu usuário"
+          placeholder="Email"
           value={userName}
           onChange={(e) => setUserName(e.target.value)}
+          className="rounded-md p-2 focus:outline-none focus:border-zinc-900 focus:border mb-4"
         />
-        <button type="submit">Entrar</button>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="********"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="rounded-md p-2 focus:outline-none focus:border-zinc-900 focus:border"
+        />
+        <button
+          type="submit"
+          className="mt-auto w-full h-12 bg-zinc-700 rounded-md text-slate-50 disabled:cursor-not-allowed disabled:opacity-20"
+          disabled={!userName || !password}
+        >
+          Entrar
+        </button>
       </form>
     </div>
   );
